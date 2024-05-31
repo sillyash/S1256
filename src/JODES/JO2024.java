5d6901c30c9906a030e10c214735e53bc9d4f076
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-05-31 12:17:07 +0200

    aaa

--- src/JODES/JO2024.java
+++ src/JODES/JO2024.java
@@ -33,6 +33,14 @@ public class JO2024 {
 		this.sesUtilisateurs = sesUtilisateurs;
 	}
 	
+	public void addEntite(Entite entite) {
+		this.sesEntites.add(entite);
+	}
+	
+	public void removeEntite(Entite entite) {
+		this.sesEntites.remove(entite);
+	}
+	
 	// ----------- Methods -----------
 	
 	protected int demandeConnexion(String login, String motDePasse) {
@@ -41,8 +49,14 @@ public class JO2024 {
 	}
 
 	protected Entite rechercheEntite(String nomEntite) {
-		// TODO - implement JO2024.rechercheEntite
-		throw new UnsupportedOperationException();
+		nomEntite = nomEntite.toUpperCase();
+		for (Entite entite : this.sesEntites) {
+			if (entite.getNom().toUpperCase().contains(nomEntite))
+			{
+				return entite;
+			}
+		}
+		return null;
 	}
 	
 	protected Utilisateur rechercheUtilisateur(String login) {
@@ -51,13 +65,35 @@ public class JO2024 {
 	}
 
 	protected Entite rechercheEntite(String nomEntite, String typeEntite) {
-		// TODO - implement JO2024.rechercheEntite
-		throw new UnsupportedOperationException();
+		nomEntite = nomEntite.toUpperCase();
+		for (Entite entite : this.sesEntites) {
+			System.out.println(entite.getClass());
+			if (entite.getClass().toString() == typeEntite)
+			{
+				if (entite.getNom().toUpperCase().contains(nomEntite))
+				{
+					return entite;
+				}
+			}
+			
+		}
+		return null;
 	}
 	
 	protected void afficherPlanningSemaine(int numeroSemaine) {
 		// TODO - implement JO2024.afficherPlanningSemaine
 		throw new UnsupportedOperationException();
+	}
+	
+	public static void main(String args[])
+	{
+		JO2024 JO = new JO2024();
+		Pays FRA = new Pays("France", "FRA");
+		Athlete Alain = new Athlete("Alain", "Sandoz", FRA);
+		
+		JO.addEntite(Alain);
+		
+		JO.rechercheEntite("alain", "");
 	}
 
 }
