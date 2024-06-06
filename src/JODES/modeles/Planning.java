5c4f9837b405eabbc5b54c87e7932500e98cc964
Ashley <swannmerienne@gmail.com>
2024-06-06 21:35:46 +0200

    finished planning ????

--- src/JODES/modeles/Planning.java
+++ src/JODES/modeles/Planning.java
@@ -14,43 +14,55 @@ import JODES.JO2024;
 public class Planning {
 
     protected int numSemaine;
-    protected ArrayList<List<LocalTime>> days = new ArrayList<>();
+    protected List<List<Session>> sessions = new ArrayList<List<Session>>();
+    protected ArrayList<LocalDate> days = new ArrayList<>();
+    protected int year;
+    protected int month;
+    protected int day;
 
     // ----------- Constructors -----------
 
     public Planning(LocalDate date) {
         this.numSemaine = getNumSemaineFromDate(date);
+        this.year = date.getYear();
+        this.month = date.getMonthValue();
+        this.day = date.getDayOfMonth();
         initializeDaysOfWeek();
     }
 
     // ----------- Getters & setters -----------
     
-    public int getNumSemaine() {
-        return numSemaine;
-    }
-
-    public void setNumSemaine(int numSemaine) {
-        this.numSemaine = numSemaine;
-    }
-
-    public ArrayList<List<LocalTime>> getDays() {
-        return days;
-    }
-
-    public void setDays(ArrayList<List<LocalTime>> days) {
-        this.days = days;
-    }
-
-    public List<LocalTime> getDay(int dayNum) {
-        if (dayNum < 0 || dayNum > 6) return new List<LocalTime>();
-    }
-
-    // ----------- Methods -----------
-
+    
     public int getNumSemaineFromDate(LocalDate d) {
         // from StackOverflow
         return d.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
     }
+
+    public int getNumSemaine() {
+        return numSemaine;
+    }
+
+    public List<List<Session>> getSessions() {
+        return sessions;
+    }
+
+    public ArrayList<LocalDate> getDays() {
+        return days;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    // ----------- Methods -----------
 
     public void getSessionsFromDataBase(int numSemaine) {
         for (Entite e : JO2024.sesEntites)
@@ -61,7 +73,7 @@ public class Planning {
                 int numSemaineSession = getNumSemaineFromDate(s.getDate());
                 if (numSemaineSession == this.numSemaine)
                 {
-                    this.sessions.add(s);
+                    this.sessions.get(0).add(s);
                 }
             }
         }
@@ -69,8 +81,13 @@ public class Planning {
 
     public void initializeDaysOfWeek() {
         WeekFields week = WeekFields.of(Locale.FRANCE);
-        LocaleDate date;
-        this.days.
+        for (int i=0; i<7; i++)
+        {
+            LocalDate date = LocalDate.of(year, month, day)
+            .with(week.weekOfYear(), numSemaine)
+            .with(week.dayOfWeek(), i);
+            this.days.add(date);
+        }
     }
-    
+
 }
