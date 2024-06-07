24cc11d621d45144d0ba79e3fcb344a0df934149
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-06-07 11:00:19 +0200

    Finished select controllers for viws

--- src/JODES/controleurs/ControleurBtnSelectSession.java
+++ src/JODES/controleurs/ControleurBtnSelectSession.java
@@ -1,22 +1,22 @@
 package JODES.controleurs;
 import JODES.JO2024;
-import JODES.modeles.Epreuve;
-import JODES.vues.ComboBoxEpreuve;
+import JODES.modeles.Session;
+import JODES.vues.ComboBoxSession;
 import JODES.vues.MessageBox;
-import JODES.vues.ModifierEpreuve;
+import JODES.vues.ModifierSession;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
 
-public class ControleurBtnSelectEpreuve implements ActionListener  {
+public class ControleurBtnSelectSession implements ActionListener  {
 
-    protected Epreuve epreuve;
-    protected ComboBoxEpreuve CMB;
+    protected Session session;
+    protected ComboBoxSession CMB;
     protected int option;
     public static final int MODIF = 0;
     public static final int SUPPR = 1;
 
-    public ControleurBtnSelectEpreuve(ComboBoxEpreuve combo, int option) {
+    public ControleurBtnSelectSession(ComboBoxSession combo, int option) {
         this.CMB = combo;
         if (option == MODIF || option == SUPPR) this.option = option;
         else this.option = MODIF;
@@ -26,17 +26,17 @@ public class ControleurBtnSelectEpreuve implements ActionListener  {
     public void actionPerformed(ActionEvent e) {
         if (this.CMB.getSelectedItem() == "") {
             new MessageBox(
-                "Vous devez sélectionner une épreuve !",
+                "Vous devez sélectionner une équipe !",
                 "Champ obligatoire",
                 JOptionPane.YES_NO_CANCEL_OPTION);
         }
         else {
             String nomEpreuve = (String)CMB.getSelectedItem();
-            this.epreuve = (Epreuve)JO2024.rechercheEntite(nomEpreuve);
+            this.session = (Session)JO2024.rechercheEntite(nomEpreuve);
             if (this.option == MODIF) {
-                new ModifierEpreuve(this.epreuve);
+                new ModifierSession(this.session);
             } else {
-                JO2024.removeEntite(epreuve);
+                JO2024.removeEntite(session);
             };
         }
     }
---
+++ src/JODES/vues/ComboBoxEquipe.java
@@ -0,0 +1,27 @@
+package JODES.vues;
+import JODES.modeles.Equipe;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+
+public class ComboBoxEquipe extends JComboBox<String> {
+    
+    protected ArrayList<Equipe> listeEquipes;
+
+    public ComboBoxEquipe(ArrayList<Equipe> listeEquipes) {
+        this.listeEquipes = listeEquipes;
+        addItems();
+    }
+
+    protected void addItems() {
+        this.addItem("");
+        for (Equipe e : listeEquipes)
+        {
+            this.addItem(e.getNom());
+        }
+    }
+
+    public ArrayList<Equipe> getItemList() {
+        return listeEquipes;
+    }
+    
+}
