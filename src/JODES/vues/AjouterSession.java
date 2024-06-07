b9f45483a35ff4db2734e3261ba9cbf6a0338235
Ash Merienne <ash.merienne@universite-paris-saclay.fr>
2024-06-07 09:38:02 +0200

    update Session views

--- src/JODES/vues/AjouterSession.java
+++ src/JODES/vues/AjouterSession.java
@@ -1,14 +1,49 @@
 package JODES.vues;
+import javax.swing.*;
+import JODES.JO2024;
+import JODES.controleurs.ControleurBTNRetour;
+import JODES.controleurs.RetourVue;
 import JODES.modeles.Session;
 
-public class AjouterSession extends ModifierSession {
+import java.awt.*;
 
-	public AjouterSession() {
-        super(new Session("", "", null, null, null, null, null, null, null),true);   
-    }
+public class AjouterSession extends JFrame implements RetourVue{
 
-    public static void main () {
-        new AjouterSession();
+    protected Session sessionModele;
+
+	public AjouterSession(Session sessionModele,boolean isNew) {
+        super("JODES");
+        this.sessionModele = sessionModele;
+        
+        PanelTitle panelTitle = new PanelTitle("Session Création");
+        JButton button = new JButton("Retour" + "\u21A9");
+        ControleurBTNRetour btnretour = new ControleurBTNRetour(this);
+        add(button, BorderLayout.SOUTH);
+        JButton buttonSave = new JButton("Sauvegarder et quitter");
+      
+        add(panelTitle, BorderLayout.NORTH);
+        button.addActionListener(btnretour);
+        
+        JPanel panelSaveRetour = new JPanel();
+        panelSaveRetour.setLayout(new GridLayout(2,1));
+        panelSaveRetour.add(buttonSave);
+        panelSaveRetour.add(button);
+        add(panelSaveRetour,BorderLayout.SOUTH);
+        
+        
+        JPanel panelDuMilieu = new JPanel();
+        panelDuMilieu.setLayout(new GridLayout(3,2));
+        panelDuMilieu.add(new GridFormField(new JTextField(sessionModele.getNom()),new JLabel("Nom Epreuve :")));
+		panelDuMilieu.add(new GridFormField(new ComboBoxHoraires(),new JLabel("Horaire Debut :")));
+		panelDuMilieu.add(new GridFormField(new ComboBoxHoraires(),new JLabel("Horaire Fin :")));
+        panelDuMilieu.add(new GridFormField(new ComboBoxEpreuve(JO2024.getEpreuves()),new JLabel("Session :")));
+		
+		add(panelDuMilieu,BorderLayout.CENTER);
+        
+      
+        setSize(800, 450);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     //Nicolas
