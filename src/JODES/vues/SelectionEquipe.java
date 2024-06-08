a4d3ac7e411de8d7ae9b7731e795c3f42d791514
Nicolas <nicolas.vouilloux@universite-paris-saclay.fr>
2024-06-08 13:29:32 +0200

    truc

--- src/JODES/vues/SelectionEquipe.java
+++ src/JODES/vues/SelectionEquipe.java
@@ -86,11 +86,11 @@ public class SelectionEquipe extends JFrame implements RetourVue,SelectionVue {
 	public void selection() {
 		if (combo.isSelectedNull())
 			javax.swing.JOptionPane.showMessageDialog(null,"Erreur Entite Null"); 
-		else 
+		else {
 			new ModifierEquipe((Equipe) combo.getSelectedEntite(), admin);
-		this.dispose();
+			this.dispose();
+		}
 	}
-	
 }
 
 
