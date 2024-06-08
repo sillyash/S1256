d7924daa59fb51a3e526ef4e0234bcc4e0622128
Emma <emma.escoffier@universite-paris-saclay.fr>
2024-06-08 19:39:29 +0200

    added same code for both arrows

--- src/JODES/controleurs/ControleurBtnFlecheForwardPlanning.java
+++ src/JODES/controleurs/ControleurBtnFlecheForwardPlanning.java
@@ -3,21 +3,21 @@ package JODES.controleurs;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
+import JODES.vues.PlanningFrame;
 import JODES.modeles.Planning;
 
 public class ControleurBtnFlecheForwardPlanning implements ActionListener{
-	protected JFrame _vue;
+	protected PlanningFrame _vue;
 	protected Planning _modele;
 	
-	public ControleurBtnFlecheForwardPlanning(Planning modele, JFrame vue) {
+	public ControleurBtnFlecheForwardPlanning(Planning modele, PlanningFrame vue) {
 		this._modele = modele;
 		this._vue = vue;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		new Planning(_modele.getDays().get(0).plusDays(7));
+		_vue.set_modele(new Planning(_modele.getDays().get(0).plusDays(7)));
 		// TODO fix this not working
 	}
 }
