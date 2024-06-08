a841e46f96be0e7a287e29549e928449ad1ccbff
Emma <emma.escoffier@universite-paris-saclay.fr>
2024-06-08 15:51:52 +0200

    noithing relly

--- src/JODES/controleurs/ControleurBtnHelpPlanning.java
+++ src/JODES/controleurs/ControleurBtnHelpPlanning.java
@@ -2,20 +2,13 @@
 package JODES.controleurs;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
 import JODES.vues.MessageBox;
 
 public class ControleurBtnHelpPlanning implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		MessageBox helpMessageBox = new MessageBox("Pour ajouter les epreuves", "Aide");
-		helpMessageBox.showMessageBox();
-	}
-
-	public static void main(String[] args) {
-		MessageBox a = new MessageBox("Pour ajouter les epreuves", "Aide");
-		a.showMessageBox();
+		MessageBox helpMessageBox = new MessageBox("", "");
+		helpMessageBox.showMessageBoxPlann();
 	}
 }
