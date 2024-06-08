2ece41fe14fdec6ec8eb2792c85ad68c2247f8f5
Nicolas <nicolas.vouilloux@universite-paris-saclay.fr>
2024-06-08 15:30:39 +0200

    savequit

--- src/JODES/vues/AjouterAthlete.java
+++ src/JODES/vues/AjouterAthlete.java
@@ -11,6 +11,11 @@ public class AjouterAthlete extends JFrame implements SauvegarderQuitter{
 
 	private static final long serialVersionUID = 1L;
 	Administrateur admin;
+	JTextField TXNom = new JTextField("");
+	JTextField TXPrenom =new JTextField("");
+	JTextField TXBiographie =new JTextField("");
+	ComboBoxPays CMBP = new ComboBoxPays(JO2024.getPays());
+	
 	public AjouterAthlete(Administrateur admin) {
         super("JODES");
         
@@ -24,10 +29,10 @@ public class AjouterAthlete extends JFrame implements SauvegarderQuitter{
         
         JPanel panelDuMilieu = new JPanel();
         panelDuMilieu.setLayout(new GridLayout(3,2));
-        panelDuMilieu.add(new GridFormField(new JTextField(""),new JLabel("Nom :")));
-		panelDuMilieu.add(new GridFormField(new JTextField(""),new JLabel("Prenom :")));
-		panelDuMilieu.add(new GridFormField(new ComboBoxPays(JO2024.getPays()),new JLabel("Pays :")));
-        panelDuMilieu.add(new GridFormField(new JTextField(""),new JLabel("Biographie en 3 mots max :")));
+        panelDuMilieu.add(new GridFormField(TXNom,new JLabel("Nom :")));
+		panelDuMilieu.add(new GridFormField(TXPrenom,new JLabel("Prenom :")));
+		panelDuMilieu.add(new GridFormField(CMBP,new JLabel("Pays :")));
+        panelDuMilieu.add(new GridFormField(TXBiographie,new JLabel("Biographie en 3 mots max :")));
 		add(panelDuMilieu,BorderLayout.CENTER);
       
         setSize(800, 450);
@@ -41,7 +46,18 @@ public class AjouterAthlete extends JFrame implements SauvegarderQuitter{
     
 	@Override
 	public void saveQuit() {
-		
+		if ((TXNom.getText()==""))
+			javax.swing.JOptionPane.showMessageDialog(null,"Erreur Entite Null");
+		else if ((TXPrenom.getText()==""))
+			javax.swing.JOptionPane.showMessageDialog(null,"Erreur Entite Null");
+		else if ((TXBiographie.getText()==""))
+			javax.swing.JOptionPane.showMessageDialog(null,"Erreur Entite Null");
+		else if ((CMBP).isSelectedNull())
+			javax.swing.JOptionPane.showMessageDialog(null,"Erreur Entite Null");
+		else 
+			//TODO enregistrer les information
+			new EpreuveFrame(admin);
+			(this).dispose();
 		
 	}
 
